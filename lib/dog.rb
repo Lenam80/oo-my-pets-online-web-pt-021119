dd73a51d388cef54ac9367c06604c4b9650f8d5c
Angelina Montanez <angelinamontanez80@gmail.com>
2019-03-23 23:17:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,6 +2,19 @@ class Dog
 attr_accessor :dog
 
 describe dog do
-  
-end
-end
+  let(:dog) {Dog.new("Daisy") }
+  end
+
+it "initializes with a name" do
+  expect(dog.name).to eq("Daisy")
+  end
+
+it "can't change its name"  do
+expect { dog.name = "Fido" }.to raise_error NoMethodError  
+  end
+
+it "can change its mood" do
+  dog.mood = ("hungry")
+  expect(dog.mood).to eq("hungry")
+  end
+end 
